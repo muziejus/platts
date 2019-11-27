e468f9eae16a4a277e97c2b670b155f3cd8ea869
Moacir P. de Sá Pereira <github@moacir.moacir.com>
2019-11-27 15:56:39 -0500

    feat: read in single html page and generate headwords.

---
+++ src/convert-html-to-data/index.js
@@ -0,0 +1,28 @@
+const fs = require("fs");
+const cheerio = require("cheerio");
+
+/*
+ * How to scrape the DSAL version of Platts:
+ * 
+ * Run a for loop:
+ *
+ * for i ({1..1254})
+ * wget https://dsal.uchicago.edu/cgi-bin/app/platts_query.py\?page\=$i ; sleep 0.1
+ *
+ * This saves a bunch of files that are named something like: platts_query.py?page=1254
+ *
+ * To change those into html files:
+ *
+ * ls -1 | sed 's/^.*=\(.*\)$/mv & page-\1.html/' | sh
+ *
+ */
+
+const file = fs.readFileSync("./original-wget/page-1.html", { encoding: "UTF-8" });
+const $ = cheerio.load(file);
+// const firstWord = $(".hw_result div").first().text();
+// console.log(firstWord);
+$(".hw_result div").each((index, element) => {
+  const el = cheerio.load(element);
+  const hw = el("hw");
+  console.log("HEADWORD: ", hw.text());
+});
